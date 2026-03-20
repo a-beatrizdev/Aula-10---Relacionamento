2ae26f28e6af709561fa1d28f26f69e492ffab6b
Alana <alanabeatriztobias>
2026-03-20 14:14:34 -0300

    Criando Classes

--- main.py
+++ main.py
@@ -8,3 +8,34 @@ Base = declarative_base()
 # Tabelas do banco
 class Usuario(Base):
     __tablename__ = "usuarios"
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    nome = Column(String(100), nullable=False)
+
+    def __init__(self, nome):
+        self.nome = nome
+    
+    #Função para imprimir
+    def __repr__(self):
+        return f"Usuario - id={self.id} - nome={self.nome}"
+    
+class Pedido(Base):
+    __tablename__ = "pedidos"
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    produto = Column(String(100), nullable=False)
+
+    def __init__(self, produto):
+        self.produto = produto
+
+    def __repr__(self):
+        return f"Pedido - id={self.id} - Produto={self.produto}"
+    
+
+    
+    
+
+
+
+
+
+
